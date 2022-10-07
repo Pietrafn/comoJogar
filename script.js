22d0288704b7d8d19771eeffcdec5650863d3b32
pietrafn <71992173+Pietrafn@users.noreply.github.com>
2022-10-06 22:43:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var metalCrusher = document.querySelector("#mettaton");
+metalCrusher.play();
+document.querySelector("#voltar").addEventListener("click", function () {
+  window.location.href = "https://codepen.io/19P1/full/qBYYQva";
+  return false;
+});
